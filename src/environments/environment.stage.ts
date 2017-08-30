12bd11e2f280641f9e1b22c7d47e2e73029246a2
Ivan Dugalic <idugalic@gmail.com>
2017-08-30 15:39:38 +0200

    Adding 'staging deployment' job to CircleCI workflow

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  blogPostQueryBaseUrl: 'https://stage-my-company-monolith.cfapps.io//api/blogposts',
+  blogPostCommandBaseUrl: 'https://stage-my-company-monolith.cfapps.io//api/blogpostcommands',
+  projectsQueryBaseUrl: 'https://stage-my-company-monolith.cfapps.io/api/projects',
+  projectsCommandBaseUrl: 'https://stage-my-company-monolith.cfapps.io//api/projectcommands',
+  authTokenUrl: 'https://stage-my-company-monolith.cfapps.io//oauth/token'
+};
