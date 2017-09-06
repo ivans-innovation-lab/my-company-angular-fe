bc1b748d8f69243c1209b7b0e9605d616898e6a4
Ivan Dugalic <idugalic@gmail.com>
2017-09-06 15:20:17 +0200

    Adding more content on the page, changing style...

--- src/app/presentational-components/main/main.component.ts
+++ src/app/presentational-components/main/main.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MainComponent implements OnInit {
 
   @Input() title: string;
-
+  @Input() subTitle: string;
   constructor() { }
 
   ngOnInit() {
